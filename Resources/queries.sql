056bb836dd329871c0177107abd424ba75105057
Anuja-y <anuja.yadwadkar@gmail.com>
2023-05-21 21:41:48 +0800

    Final Submission

--- Resources/queries.sql
+++ Resources/queries.sql
@@ -1,3 +1,5 @@
+
+-- List of the employee number, last name, first name, sex, and salary of each employee 
 select 
 	employees.emp_no, 
 	employees.last_name, 
@@ -9,6 +11,7 @@ join salaries on employees.emp_no = salaries.emp_no;
 
 -------
 
+--List of the first name, last name, and hire date for the employees who were hired in 1986
 select 
 	employees.first_name, 
 	employees.last_name, 
@@ -18,6 +21,7 @@ where hire_date like '%1986%';
 
 -------
 
+--List of the manager of each department along with their department number, department name, employee number, last name, and first name
 select
   dept_manager.dept_no,
   departments.dept_name,
@@ -30,6 +34,7 @@ join employees on dept_manager.emp_no = employees.emp_no;
 
 --------
 
+--List of the department number for each employee along with that employee’s employee number, last name, first name, and department name
 select
 	dept_emp.dept_no,
 	dept_emp.emp_no,
@@ -42,6 +47,7 @@ join departments on dept_emp.dept_no = departments.dept_no;
 
 --------
 
+--List of first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B
 select 
 	employees.first_name, 
 	employees.last_name,
@@ -52,6 +58,7 @@ and last_name like 'B%';
 
 --------
 
+-- List of each employee in the Sales department, including their employee number, last name, and first name
 select 
 	employees.emp_no,
 	employees.last_name,
@@ -63,6 +70,7 @@ where departments.dept_name = 'Sales';
 
 ---------
 
+--List of each employee in the Sales and Development departments, including their employee number, last name, first name, and department name
 select 
 	employees.emp_no,
 	employees.last_name,
@@ -75,6 +83,7 @@ where departments.dept_name in ('Sales', 'Development');
 
 ----------
 
+--List of the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name) 
 select last_name,
 count(*) as frequency
 from employees
